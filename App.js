f0eb98f982605df0e9791f6459083b837c7b3978
Gn0lee <jinho605@gmail.com>
2023-11-05 21:33:28 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text,  View, ScrollView, Dimensions } from 'react-native';
+
+const { width : SCREEN_WIDTH } = Dimensions.get('window')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <ScrollView contentContainerStyle={styles.weather} horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato'
   },
+  city : {
+    flex: 1.2,
+    backgroundColor: 'tomato',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName : {
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather: {
+    // flex: 3,
+  },
+  day: {
+    // flex:1,
+    width: SCREEN_WIDTH,
+    alignItems:'center'
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 10
+  },
+  description : {
+    fontSize: 60
+  }
 });
